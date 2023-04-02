0f746bf4fb43280ab9d8075bed8a982fe669b3a9
Lars Liedtke <lars@familie-liedtke.net>
2023-04-02 19:26:04 +0200

    add test for sending a message and checking via API, if it arrived

--- tests/integration/test_http_messages.py
+++ tests/integration/test_http_messages.py
@@ -1,13 +1,37 @@
-import unittest
-import mailpit.api
+import email
+import logging
+import smtplib
 
 import tests.integration
 
-class TestMessages(unittest.TestCase):
-    def setUp(self) -> None:
-        self.api = mailpit.api.API("http://mailpit:8025")
 
 class TestMessages(tests.integration.MailpitClientIntegrationCase):
     def test_messages_endpoint__empty(self):
         messages = self.api.get_messages()
         self.assertEqual(len(messages.messages), 0)
+
+    def test_messages_endpoint__sendmessage(self):
+        logger = logging.getLogger(
+            "tests.integration.test_messages_endpoint__sendmessage"
+        )
+        logger.info("connecting to smtp_server")
+        self.smtp_server = smtplib.SMTP("mailpit", 1025)
+        logger.info("reading mail from file")
+        with open(f"{self.PROJECT_PATH}/tests/mail/email.eml") as fp:
+            mail = email.message_from_file(fp)
+        logger.info("sending message")
+        self.smtp_server.send_message(
+            mail,
+            from_addr="Sender Smith <sender@example.com>",
+            to_addrs="Recipient Ross <recipient@example.com>",
+        )
+        logger.info("retrieving messages via API-endpoint")
+        messages = self.api.get_messages()
+
+        logger.info("closing smtp connection")
+        self.smtp_server.quit()
+        self.smtp_server.close()
+
+        logger.info("checking asserts")
+        self.assertEqual(1, messages.count)
+        self.assertEqual(1, len(messages.messages))
