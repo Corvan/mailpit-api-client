7cf80d237e281ac0b98e1779f4e8d74e895ed262
Lars Liedtke <lal@solute.de>
2023-04-28 18:02:44 +0200

    add skeletons for message testing

--- tests/integration/test_http_message.py
+++ tests/integration/test_http_message.py
@@ -1,7 +1,11 @@
-import unittest
 
+class TestMessage:
 
-class TestMessage(unittest.TestCase):
-    
-    def test_message(self):
-        self.assertTrue(True)
+    def test_get_message(self, log, smtp_server, message):
+        log.info("testing get")
+
+    def test_get_message_attachment(self, log, smtp_server, message):
+        log.info("testing attachment")
+
+    def test_get_message_headers(self, log, smtp_server, message):
+        log.info("testing headers")
