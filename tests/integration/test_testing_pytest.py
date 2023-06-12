c85e3f9ec452e5499317bb4efc0a1b0481d8e176
Lars Liedtke <lars@familie-liedtke.net>
2023-06-12 02:20:58 +0200

    testing: add first pytest fixture

---
+++ tests/integration/test_testing_pytest.py
@@ -0,0 +1,19 @@
+def test_pytest_helpers(pytester):
+    pytester.makeconftest("""pytest_plugins = ["mailpit.testing.pytest"]""")
+    pytester.makepyfile(
+        """import pytest as _pytest
+import os as _os
+import pathlib as _pathlib
+
+if _pathlib.Path(_os.environ["HOME"]).is_relative_to(_pathlib.Path("/tmp/pytest-of-root")):
+    api_url = "http://mailpit:8025"
+else:
+    api_url = "http://localhost:8025"
+
+@_pytest.mark.parametrize("mailpit_api", [api_url], indirect=True)
+def test_api(mailpit_api):
+    assert len(mailpit_api.get_messages().messages) == 0
+"""
+    )
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)
