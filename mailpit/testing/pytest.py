34832d4d21fe86dd4f3c1c2afa4dcba379c5016c
Lars Liedtke <lars@familie-liedtke.net>
2023-07-08 15:23:59 +0200

    pytest: add default for mailpit_api fixture

--- mailpit/testing/pytest.py
+++ mailpit/testing/pytest.py
@@ -1,4 +1,4 @@
-"""Module providing helpers for :py:module:`unittest` kind of testing against the
+"""Module providing helpers for :py:mod:`unittest` kind of testing against the
 Mailpit-API"""
 
 try:
@@ -7,6 +7,8 @@ except ImportError:
     _pytest = None
 
 if _pytest:
+    import pathlib as _pathlib
+    import os as _os
     import mailpit.client.api as _api
     import mailpit.client.models as _models
 
@@ -15,7 +17,15 @@ if _pytest:
         """fixture creating a connection to the mailpit API. This fixture is meant to be
         called parametrized, in order to pass the url for which an
         :py:class:`mailpit.client.api.API` instance is created and yielded"""
-        client_api = _api.API(request.param)
+        try:
+            client_api = _api.API(request.param)
+        except AttributeError:
+            if _pathlib.Path(_os.environ["HOME"]).is_relative_to(
+                _pathlib.Path("/tmp/pytest-of-root")
+            ):
+                client_api = _api.API("http://mailpit:8025")
+            else:
+                client_api = _api.API("http://localhost:8025")
 
         yield client_api
 
--- tests/integration/test_testing_pytest.py
+++ tests/integration/test_testing_pytest.py
@@ -17,3 +17,21 @@ def test_api(mailpit_api):
     )
     result = pytester.runpytest()
     result.assert_outcomes(passed=1)
+
+
+def test_pytest_helpers_default_api_url(pytester):
+    pytester.makeconftest("""pytest_plugins = ["mailpit.testing.pytest"]""")
+    pytester.makepyfile(
+        """import pytest as _pytest
+import os as _os
+import pathlib as _pathlib
+
+def test_api(mailpit_api):
+    if _pathlib.Path(_os.environ["HOME"]).is_relative_to(_pathlib.Path("/tmp/pytest-of-root")):
+        assert mailpit_api.mailpit_url == "http://mailpit:8025"
+    else:
+        assert mailpit_api.mailpit_url == "http://localhost:8025"
+"""
+    )
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)
